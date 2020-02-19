583dd35a9362197831fc6d6330ff2a53a8a24373
anonymous <anonymous>
2020-02-19 16:40:34 -0500

    Level 3 - Initial work completed for checking Duplicate Emails

--- app/controllers/api/v1/person_controller.rb
+++ app/controllers/api/v1/person_controller.rb
@@ -9,32 +9,42 @@ module Api
 			end
 
 			def retrievePeople
-				uri = URI('https://api.salesloft.com/v2/people.json')
-        http = Net::HTTP.new(uri.host, uri.port)
-        request = Net::HTTP::Get.new(uri)
-        request["Authorization"] = "Bearer #{ENV['SALESLOFT_APPLICATION_SECRET']}"
-        http.use_ssl = true
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        response = http.request(request)
-        response_body = JSON.parse(response.body)
-        render json: {response: response_body, response_code: response.code}
+				response_body = makeApiRequest
+        render json: {response: response_body, response_code: 200}
 			end
 
 			def characterCount
 				characterHash = {}
-				uri = URI('https://api.salesloft.com/v2/people.json')
-        http = Net::HTTP.new(uri.host, uri.port)
-        request = Net::HTTP::Get.new(uri)
-        request["Authorization"] = "Bearer #{ENV['SALESLOFT_APPLICATION_SECRET']}"
-        http.use_ssl = true
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        response = http.request(request)
-        response_body = JSON.parse(response.body)
+				response_body = makeApiRequest
         response_body["data"].each do |person|
         	retrieveCharacterCount(person["email_address"].downcase, characterHash)
       	end
       	characterHash = Hash[characterHash.sort_by{|k, v| v}.reverse]
         render json: {response: characterHash, response_code: 200}
+			end
+
+			def duplicates
+				response_body = makeApiRequest
+				# Extract all emails without whitespace remove everything after @ and downcase
+        response_body["data"].map!{|k| k["email_address"].downcase.gsub(/[[:space:]]/, '')}
+        # Based on Documentation only looking for username I would remove this code if email was a factor
+        response_body["data"].map! {|email|
+        	symbol_index = email.index('@')
+        	#email.slice!(symbol_index-1..email.length)
+        	email = {
+        			email_address: email,
+        			username: username = email[0..symbol_index-1],
+        			domain: email[symbol_index..email.length]
+        		}
+        }
+        return_hash = checkDupe(response_body["data"])
+
+        # Check Character Range 
+        	# if range < 2
+        		 # if substring.exist email2 
+        		  # add dupe
+
+        render json: {response: return_hash, response_code: 200}
 			end
 
 			# ***************** Private Methods ********************
@@ -48,7 +58,41 @@ module Api
   					characterHash[c] = 1
   				end
 				}
-	    end # End makeRequest
+	    end # End retrieveCharacterCount
+
+	    def checkDupe(email_arr)
+	    	dupeArr = []
+	    	copy_arr = Array.new(email_arr)
+	    	email_arr.each do |selected_obj|
+	    		copy_arr.each do |copied_obj|                
+	    			if selected_obj[:username] != copied_obj[:username]
+	    					if selected_obj[:username].include? copied_obj[:username]
+	    							length_range = Range.new(copied_obj[:username].length,copied_obj[:username].length+2)
+	    							if length_range.cover? selected_obj[:username].length
+	    									dupeArr.push(
+	    										{original_email: selected_obj, duplicate_email: copied_obj}
+	    										)
+	    							end
+	    					end
+	    			end
+	    		end
+	    	end
+	    	return dupeArr
+
+	    end # End checkDupe
+
+	    def makeApiRequest
+	    	uri = URI('https://api.salesloft.com/v2/people.json')
+        http = Net::HTTP.new(uri.host, uri.port)
+        request = Net::HTTP::Get.new(uri)
+        request["Authorization"] = "Bearer #{ENV['SALESLOFT_APPLICATION_SECRET']}"
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        response = http.request(request)
+        response_body = JSON.parse(response.body)
+        return response_body
+	    end # End makeApiRequest
+
 		end # End Class PersonController
   end # End module V1
 end # End module Api
